a04373a7312bd30d918b2ab85b451a991494acea
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-06-27 16:30:49 +0100

    Configure simplecov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,3 +1,5 @@
+require 'simplecov'
+SimpleCov.start
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
@@ -26,6 +28,8 @@ require 'capybara'
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
+
+SimpleCov.formatter = SimpleCov::Formatter::Console
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
