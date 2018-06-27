ee9e86d5fe59229d661e3e93aa512d9ea9110c28
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-06-27 17:45:11 +0100

    Begin testing api request

--- app/controllers/mmd_controller.rb
+++ app/controllers/mmd_controller.rb
@@ -4,6 +4,7 @@ require 'json'
 class MmdController < ApplicationController
 
   def index
+    # This logic should be in the model.
     url = "https://stark-brook-82296.herokuapp.com/events?date=#{params[:date]}"
     uri = URI(url)
     response = Net::HTTP.get(uri)
