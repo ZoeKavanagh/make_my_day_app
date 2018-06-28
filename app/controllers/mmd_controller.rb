6bb5209c35370ad7c950586d918c4ce102a3b0cf
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-06-28 10:46:09 +0100

    Test ApiRequest make_request method

--- app/controllers/mmd_controller.rb
+++ app/controllers/mmd_controller.rb
@@ -4,10 +4,6 @@ require 'json'
 class MmdController < ApplicationController
 
   def index
-    # This logic should be in the model.
-    url = "https://stark-brook-82296.herokuapp.com/events?date=#{params[:date]}"
-    uri = URI(url)
-    response = Net::HTTP.get(uri)
-    @events = JSON.parse(response)
+    @events = ApiRequest.make_request(params[:date])
   end
 end
--- spec/controllers/mmd_controller_spec.rb
+++ spec/controllers/mmd_controller_spec.rb
@@ -1,16 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe MmdController, type: :controller do
-  let(:url) { "https://example.com" }
-  before { allow(URI).to receive(:url).and_return(:uri) }
-  let(:uri) { double :uri }
-  before { allow(Net::HTTP).to receive(:get).and_return(:response) }
-  before { allow(JSON).to receive(:parse).and_return(:events) }
-  let(:events) { [{"title" => 'title', "description" => 'description', "price" => 5}] }
 
   describe('#index') do
-    it 'Returns a JSON object' do
-
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(200)
     end
   end
 end
