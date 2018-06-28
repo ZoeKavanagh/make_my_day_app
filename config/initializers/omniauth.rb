e20096eee83cf438f7a2d47be8ebfa0226429fc6
Salome Lambermont <salome.lambermont@gmail.com>
2018-06-28 10:10:25 +0100

    Fix authenticaiton credentials

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV["USER"], ENV["SECRET"], {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :google_oauth2, '27438778495-ogdjl0vs7qdu9nsglp90tvo1ikftfoun.apps.googleusercontent.com', 'gSsEn7o9ZwluOAZBelnO47n7', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
