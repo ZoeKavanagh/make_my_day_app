0d6aad7c34e5dff59c139b33baee89d90e51cc1d
Salome Lambermont <salome.lambermont@gmail.com>
2018-06-29 11:23:31 +0100

    Reset app to remove group

--- spec/features/create_new_group_spec.rb
+++ spec/features/create_new_group_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.feature 'Creating new group', type: :feature do
-  scenario 'User creates new group' do
+  scenario 'User clicks create new group button' do
     visit '/'
     click_link 'Create New Group'
     expect(page).to have_current_path(new_group_path)
--- spec/models/group_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Group, type: :model do
-  xit { expect(subject).to have_and_belong_to_many(:user) }
-end
