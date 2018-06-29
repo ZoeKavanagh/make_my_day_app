8779d908f60cb65d59e3caf378bc3afed6df0831
Salome Lambermont <salome.lambermont@gmail.com>
2018-06-29 13:25:26 +0100

    Display list of users on new group page

--- spec/features/create_new_group_spec.rb
+++ spec/features/create_new_group_spec.rb
@@ -6,4 +6,18 @@ RSpec.feature 'Creating new group', type: :feature do
     click_link 'Create New Group'
     expect(page).to have_current_path(new_group_path)
   end
+
+  scenario 'Displays a list of all users' do
+    FactoryBot.create(:user)
+    visit '/'
+    click_link 'Create New Group'
+    expect(page).to have_content('MyString')
+  end
+
+  # scenario 'User can enter group name' do
+  #   visit '/'
+  #   click_link 'Create New Group'
+  #   fill_in 'Group Name', with: 'The Group'
+  #
+  # end
 end
