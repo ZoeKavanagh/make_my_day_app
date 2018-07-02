4f9e7fb244bb7dc084d390cf59ae2a36b459cacf
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-07-02 14:39:17 +0100

    Get rid of visibility ps

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,22 +3,13 @@ class User < ApplicationRecord
   has_many :posts
 
   def self.from_omniauth(auth)
-    p '1'
     where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |u|
-      p '2'
       u.provider = auth.provider
-      p '3'
       u.uid = auth.uid
-      p '4'
       u.name = auth.info.name
-      p '5'
-      p auth
       u.oauth_token = auth.credentials.token
-      p '6'
       u.oauth_expires_at = Time.at(auth.credentials.expires_at)
-      p '7'
       u.save!
-      p '8'
     end
   end
 end
